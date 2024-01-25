055a7bcd37ca9f9e1fa4a08820d811b454e78bd9
Finlay Christ <RandomiaGaming@gmail.com>
2024-01-25 15:12:39 -0800

    Backup commit

--- Program.cs
+++ Program.cs
@@ -1,16 +1,243 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace MDRPG
 {
-    internal class Program
+    public static class Program
     {
-        static void Main(string[] args)
+        public static Texture2D playerTexture;
+        public static XNAInterface xnaInterface;
+        public static Player player;
+        public static World world;
+        [STAThread]
+        public static int Main(string[] args)
         {
+            xnaInterface = new XNAInterface(Update);
+
+            Initialize();
+
+            xnaInterface.Run();
+
+            return 0;
         }
+        public static void Initialize()
+        {
+            player = new Player(xnaInterface);
+            world = new World(xnaInterface);
+            world.Generate();
+        }
+        public static void Update()
+        {
+            world.Update();
+            player.Update();
+        }
+    }
+    public class Player
+    {
+        public int X = 0;
+        public int Y = 0;
+        public double SubPixelX = 0.0;
+        public double SubPixelY = 0.0;
+        public double VelocityX = 0.0;
+        public double VelocityY = 0.0;
+        public double AccelerationX = 0.0;
+        public double AccelerationY = 0.0;
+        public int Rotation = 0; //In range from 0 to _sprite.Width;
+
+        public const double MoveSpeed = 2.5;
+
+        private Texture2D _boarder;
+        private int _boarderWidth;
+        private int _boarderHeight;
+        private Texture2D _sprite;
+        private int _spriteWidth;
+        private XNAInterface _xnaInterface;
+        private int _cameraOffsetX;
+        private int _cameraOffsetY;
+        public Player(XNAInterface xnaInterface)
+        {
+            _xnaInterface = xnaInterface;
+
+            _boarder = xnaInterface.LoadTexture("MDRPG.PlayerBoarder.png");
+            _boarderWidth = _boarder.Width;
+            _boarderHeight = _boarder.Height;
+
+            _sprite = xnaInterface.LoadTexture("MDRPG.PlayerSprite.png");
+            if (_sprite.Height != _boarderHeight)
+            {
+                throw new Exception("Da fuck is this shit.");
+            }
+            _spriteWidth = _sprite.Width;
+
+            _cameraOffsetX = ((-1 * _xnaInterface.RenderWidth) / 2) + (_boarderWidth / 2);
+            _cameraOffsetY = ((-1 * _xnaInterface.RenderHeight) / 2) + (_boarderWidth / 2);
+        }
+        private const double Tao = 6.2831853071795862;
+        public void Update()
+        {
+            InputUpdate();
+            PhysicsUpdate();
+            CameraUpdate();
+            Render();
+        }
+        public void InputUpdate()
+        {
+            KeyboardState keyboardState = Keyboard.GetState();
+
+            if (keyboardState.IsKeyDown(Keys.D))
+            {
+                VelocityX = MoveSpeed;
+            }
+            else if (keyboardState.IsKeyDown(Keys.A))
+            {
+                VelocityX = -MoveSpeed;
+            }
+            else
+            {
+                VelocityX = 0.0;
+            }
+
+            if (keyboardState.IsKeyDown(Keys.W))
+            {
+                VelocityY = MoveSpeed;
+            }
+            else if (keyboardState.IsKeyDown(Keys.S))
+            {
+                VelocityY = -MoveSpeed;
+            }
+            else
+            {
+                VelocityY = 0.0;
+            }
+
+            MouseState mouseState = Mouse.GetState();
+
+            int mouseX = mouseState.X - (_xnaInterface.Window.ClientBounds.Width / 2);
+            int mouseY = -(mouseState.Y - (_xnaInterface.Window.ClientBounds.Height / 2));
+
+            double mouseRotation = Math.Atan2(mouseY, mouseX);
+            if (mouseRotation < 0)
+            {
+                mouseRotation = Tao + mouseRotation;
+            }
+
+            Rotation = (int)((mouseRotation / Tao) * _spriteWidth);
+        }
+        public void PhysicsUpdate()
+        {
+            VelocityX += AccelerationX;
+            VelocityY += AccelerationY;
+            SubPixelX += VelocityX;
+            SubPixelY += VelocityY;
+            X = (int)SubPixelX;
+            Y = (int)SubPixelY;
+        }
+        public void CameraUpdate()
+        {
+            _xnaInterface.CameraX = X + _cameraOffsetX;
+            _xnaInterface.CameraY = Y + _cameraOffsetY;
+        }
+        public void Render()
+        {
+            _xnaInterface.DrawSprite(_boarder, X, Y);
+
+            /* if (Rotation is 0)
+             {
+                 _xnaInterface.XNASpriteBatch.Draw(_sprite, new Rectangle((int)X, (int)Y, _boarderWidth, _boarderHeight), new Rectangle(0, 0, _boarderWidth, _boarderHeight), Color.White);
+             }
+             else if (Rotation >= 16)
+             {
+                 _xnaInterface.XNASpriteBatch.Draw(_sprite, new Rectangle((int)X, (int)Y, _boarderWidth, _boarderHeight), new Rectangle(_spriteWidth - Rotation, 0, _boarderWidth, _boarderHeight), Color.White);
+             }
+             else
+             {
+                 _xnaInterface.XNASpriteBatch.Draw(_sprite, new Rectangle((int)X + Rotation, (int)Y, _boarderWidth - Rotation, _boarderHeight), new Rectangle(0, 0, _boarderWidth - Rotation, _boarderHeight), Color.White);
+                 _xnaInterface.XNASpriteBatch.Draw(_sprite, new Rectangle((int)X, (int)Y, Rotation, _boarderHeight), new Rectangle(_spriteWidth - Rotation, 0, Rotation, _boarderHeight), Color.White);
+             }*/
+        }
+    }
+    public class World
+    {
+        private Random RNG = new Random((int)DateTime.Now.Ticks);
+
+        public const int Width = 64;
+        public const int Height = 64;
+        public Tile[][] TileData;
+        public World()
+        {
+            TileData = new Tile[Width][];
+            for (int column = 0; column < Width; column++)
+            {
+                TileData[column] = new Tile[Height];
+            }
+        }
+        public Tile GroundTile;
+        public Tile SandTile;
+        private XNAInterface _xnaInterface;
+        public World(XNAInterface xnaInterface)
+        {
+            _xnaInterface = xnaInterface;
+
+            GroundTile = new Tile();
+            GroundTile.Texture = _xnaInterface.LoadTexture("MDRPG.GroundTile.png");
+            GroundTile.SeedChance = 0.05;
+            GroundTile.SpreadChance = 1;
+
+            SandTile = new Tile();
+            SandTile.Texture = _xnaInterface.LoadTexture("MDRPG.SandTile.png");
+            SandTile.SeedChance = 0.01;
+            SandTile.SpreadChance = 1;
+        }
+        public void Generate()
+        {
+            int totalTileCount = Width * Height;
+            int generatedCount = 0;
+
+
+            TileData = new Tile[Width][];
+            for (int x = 0; x < Width; x++)
+            {
+                TileData[x] = new Tile[Height];
+                for (int y = 0; y < Height; y++)
+                {
+                    if (RNG.NextDouble() < GroundTile.SeedChance)
+                    {
+                        TileData[x][y] = GroundTile;
+                    }
+                    else if (RNG.NextDouble() < SandTile.SeedChance)
+                    {
+                        TileData[x][y] = SandTile;
+                    }
+                }
+            }
+        }
+        public void Update()
+        {
+            for (int x = 0; x < Width; x++)
+            {
+                for (int y = 0; y < Height; y++)
+                {
+                    Tile tile = TileData[x][y];
+                    if (tile != null)
+                    {
+                        _xnaInterface.DrawSprite(tile.Texture, x * 16, y * 16);
+                    }
+                }
+            }
+        }
+    }
+    public struct TileData
+    {
+        public short Temperature;
+        public Texture2D Texture;
+    }
+    public class Tile
+    {
+        public double SeedChance;
+        public double SpreadChance;
+        public Texture2D Texture;
     }
 }
---
+++ XNAInterface.cs
@@ -0,0 +1,165 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+
+namespace MDRPG
+{
+    public delegate void UpdateCallback();
+    public sealed class XNAInterface : Game
+    {
+        private static Assembly assembly = typeof(XNAInterface).Assembly;
+
+        public readonly int RenderWidth = 512;
+        public readonly int RenderHeight = 288;
+
+        public int CameraX = 0;
+        public int CameraY = 0;
+
+        public bool ProfilerEnabled = false;
+
+        public GraphicsDeviceManager XNAGraphicsDeviceManager = null;
+        public SpriteBatch XNASpriteBatch = null;
+        public RenderTarget2D XNARenderTarget = null;
+
+        private UpdateCallback _updateCallback;
+        private Stopwatch _gameTimer;
+        private long _ticksLastFrame;
+        public XNAInterface(UpdateCallback updateCallback)
+        {
+            _updateCallback = updateCallback;
+
+            //this.InactiveSleepTime = default;
+            this.TargetElapsedTime = new System.TimeSpan(10000000 / 120);
+            this.MaxElapsedTime = new System.TimeSpan(10000000 / 120);
+            this.IsFixedTimeStep = true;
+            this.IsMouseVisible = true;
+
+            XNAGraphicsDeviceManager = new GraphicsDeviceManager(this);
+            XNAGraphicsDeviceManager.GraphicsProfile = GraphicsProfile.Reach;
+            XNAGraphicsDeviceManager.SynchronizeWithVerticalRetrace = false;
+            XNAGraphicsDeviceManager.HardwareModeSwitch = false;
+            XNAGraphicsDeviceManager.IsFullScreen = false;
+            XNAGraphicsDeviceManager.PreferHalfPixelOffset = false;
+            XNAGraphicsDeviceManager.PreferredBackBufferFormat = SurfaceFormat.Color;
+            XNAGraphicsDeviceManager.SupportedOrientations = DisplayOrientation.LandscapeLeft | DisplayOrientation.LandscapeRight | DisplayOrientation.Portrait | DisplayOrientation.PortraitDown | DisplayOrientation.Unknown | DisplayOrientation.Default;
+            //GraphicsDeviceManager.PreferredBackBufferWidth = default;
+            //GraphicsDeviceManager.PreferredBackBufferHeight = default;
+            XNAGraphicsDeviceManager.ApplyChanges();
+
+            GraphicsDevice.BlendState = BlendState.AlphaBlend;
+            GraphicsDevice.DepthStencilState = DepthStencilState.None;
+            GraphicsDevice.RasterizerState = RasterizerState.CullNone;
+            GraphicsDevice.BlendFactor = Color.White;
+            GraphicsDevice.Indices = default;
+            GraphicsDevice.RasterizerState = RasterizerState.CullNone;
+            GraphicsDevice.ResourcesLost = false;
+            //GraphicsDevice.GraphicsDebug = default;
+            //GraphicsDevice.Metrics = default;
+            //GraphicsDevice.ScissorRectangle = default;
+            //GraphicsDevice.Viewport = default;
+
+            Window.AllowAltF4 = true;
+            Window.AllowUserResizing = true;
+            Window.IsBorderless = false;
+            Window.Position = new Point(GraphicsDevice.Adapter.CurrentDisplayMode.Width / 4, GraphicsDevice.Adapter.CurrentDisplayMode.Height / 4);
+            Window.Title = "MDRPG - 0.1";
+
+            XNASpriteBatch = new SpriteBatch(GraphicsDevice /*, default*/);
+            XNASpriteBatch.Name = "MDRPG Main Sprite Batch";
+            XNASpriteBatch.Tag = null;
+
+            XNARenderTarget = new RenderTarget2D(GraphicsDevice, RenderWidth, RenderHeight, false, SurfaceFormat.Color, DepthFormat.None);
+            XNARenderTarget.Name = "MDRPG Main Render Target";
+            XNARenderTarget.Tag = null;
+
+            _gameTimer = new Stopwatch();
+            _gameTimer.Start();
+        }
+
+        private bool _F11LastFrame; //Fullscreen
+        private bool _F1LastFrame; //Save Screenshot
+        private bool _F2LastFrame; //Toggle Profiler
+        protected sealed override void Draw(GameTime gameTime)
+        {
+            KeyboardState keyboardState = Keyboard.GetState();
+            bool F11 = keyboardState.IsKeyDown(Keys.F11);
+            bool F2 = keyboardState.IsKeyDown(Keys.F2);
+            bool F1 = keyboardState.IsKeyDown(Keys.F1);
+            if (F11 && !_F11LastFrame)
+            {
+                XNAGraphicsDeviceManager.ToggleFullScreen();
+            }
+            if (F2 && !_F2LastFrame)
+            {
+                ProfilerEnabled = !ProfilerEnabled;
+            }
+
+            long ticksNow = _gameTimer.ElapsedTicks;
+            long TPF = ticksNow - _ticksLastFrame;
+            double FPS = 10000000.0 / TPF;
+            if (ProfilerEnabled)
+            {
+                Console.WriteLine($"TPF {TPF} FPS {FPS}");
+            }
+            _ticksLastFrame = ticksNow;
+
+            GraphicsDevice.SetRenderTarget(XNARenderTarget);
+
+            XNASpriteBatch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend, SamplerState.PointClamp, DepthStencilState.None, RasterizerState.CullNone, null, null);
+            _updateCallback.Invoke();
+            XNASpriteBatch.End();
+
+            if (F1 && !_F1LastFrame)
+            {
+                string screenshotPath = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory) + "\\Screenshot.png";
+                SavePNG(XNARenderTarget, screenshotPath);
+                Console.WriteLine($"Screenshot saved to \"{screenshotPath}\".");
+            }
+
+            GraphicsDevice.SetRenderTarget(null);
+
+            XNASpriteBatch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend, SamplerState.PointClamp, DepthStencilState.None, RasterizerState.CullNone, null, null);
+            XNASpriteBatch.Draw(XNARenderTarget, new Rectangle(0, 0, GraphicsDevice.Viewport.Width, GraphicsDevice.Viewport.Height), new Rectangle(0, 0, RenderWidth, RenderHeight), Color.White);
+            XNASpriteBatch.End();
+
+            _F11LastFrame = F11;
+            _F2LastFrame = F2;
+            _F1LastFrame = F1;
+        }
+        public void SavePNG(RenderTarget2D target, string path)
+        {
+            FileStream fileStream = File.Open(path, FileMode.OpenOrCreate, FileAccess.Write, FileShare.None);
+            target.SaveAsPng(fileStream, target.Width, target.Height);
+            fileStream.Close();
+            fileStream.Dispose();
+        }
+        public void SavePNG(Texture2D target, string path)
+        {
+            FileStream fileStream = File.Open(path, FileMode.OpenOrCreate, FileAccess.Write, FileShare.None);
+            target.SaveAsPng(fileStream, target.Width, target.Height);
+            fileStream.Close();
+            fileStream.Dispose();
+        }
+        public void DrawSprite(Texture2D sprite, int xWorld, int yWorld)
+        {
+            int xScreen = xWorld - CameraX;
+            int yScreen = yWorld - CameraY;
+            if (xScreen < RenderWidth && yScreen < RenderHeight && xScreen + sprite.Width >= 0 && yScreen + sprite.Height >= 0)
+            {
+                int yScreenInverted = RenderHeight - (yScreen + sprite.Height);
+                XNASpriteBatch.Draw(sprite, new Vector2(xScreen, yScreenInverted), Color.White);
+            }
+        }
+        public Texture2D LoadTexture(string embeddedResourceName)
+        {
+            Stream embeddedResourceStream = assembly.GetManifestResourceStream(embeddedResourceName);
+            Texture2D output = Texture2D.FromStream(GraphicsDevice, embeddedResourceStream);
+            embeddedResourceStream.Dispose();
+            return output;
+        }
+    }
+}
